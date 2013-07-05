ee6868d90efe24ddcc9f1e550dc982885d53c708
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-07-05 13:01:56 -0300

    Set new path for sqlite file

--- project_name/settings.py
+++ project_name/settings.py
@@ -10,7 +10,7 @@ PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_PATH, '{{ project_name }}.db'),
+        'NAME': os.path.join(PROJECT_PATH, '../{{ project_name }}.db'),
     }
 }
 
