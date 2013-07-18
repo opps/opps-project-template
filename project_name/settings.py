aa93841fbe646dcd941b7077e474c4cfd4bb2f64
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-07-18 02:59:27 -0300

    Add Opps Admin on settings

--- project_name/settings.py
+++ project_name/settings.py
@@ -66,6 +66,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.redirects',
+    'opps.contrib.admin',
     'django.contrib.admin',
 
     'opps.core',
