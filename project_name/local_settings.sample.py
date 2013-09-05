4c0a82295ee6eb71da9659e6f86b1e695e2030ef
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-09-05 15:12:49 -0300

    Create local_settings sample file

---
+++ project_name/local_settings.sample.py
@@ -0,0 +1,9 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '{{ project_name }}_demo',
+        'USER': '',
+        'PASS': '',
+        'HOST': '127.0.0.1'
+    }
+}
